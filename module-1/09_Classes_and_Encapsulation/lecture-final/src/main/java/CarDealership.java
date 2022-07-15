15a0dfeefd43f1da61fe3d7d0c37468dc7b849df
joshuamack <joshua.mack@techelevator.com>
2022-07-15 14:47:13 -0400

    A whole heap of stuff.

---
+++ module-1/09_Classes_and_Encapsulation/lecture-final/src/main/java/CarDealership.java
@@ -0,0 +1,7 @@
+public class CarDealership {
+    public static void main(String[] args) {
+        Vehicle myChevy = new Vehicle("Chevy", "Donk", 1977, 30000.00, 'U', 1, 'A', 90000);
+        System.out.println(myChevy.toString());
+
+    }
+}
