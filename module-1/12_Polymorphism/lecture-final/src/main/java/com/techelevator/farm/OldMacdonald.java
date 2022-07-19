81c0faf407537b44a9e31a7ad37dc813d199dc6b
joshuamack <joshua.mack@techelevator.com>
2022-07-19 12:35:57 -0400

    Lecture final

---
+++ module-1/12_Polymorphism/lecture-final/src/main/java/com/techelevator/farm/OldMacdonald.java
@@ -0,0 +1,26 @@
+package com.techelevator.farm;
+
+public class OldMacdonald {
+	public static void main(String[] args) {
+
+		Singable[] singables = new Singable[]{ new Cow(), new Chicken(), new Tractor() };
+
+		for (Singable singable : singables) {
+			String name = singable.getName();
+			String sound = singable.getSound();
+			System.out.println("Old MacDonald had a farm, ee, ay, ee, ay, oh!");
+			System.out.println("And on his farm he had a " + name + ", ee, ay, ee, ay, oh!");
+			System.out.println("With a " + sound + " " + sound + " here");
+			System.out.println("And a " + sound + " " + sound + " there");
+			System.out.println("Here a " + sound + " there a " + sound + " everywhere a " + sound + " " + sound);
+			System.out.println();
+		}
+
+		Sellable [] sellables = new Sellable[] {new Egg(), new Pig(), new Cow()};
+
+		for (Sellable sellable : sellables){
+			System.out.println("Step right up and get your " +sellable.getName());
+			System.out.println("For the low low price of only $" + sellable.getPrice());
+		}
+	}
+}
