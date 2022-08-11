6bea1ff6f7b774ac40f059a13119e0350a0de1bf
Ken Peterson <Peterskh13@gmail.com>
2022-08-11 09:34:22 -0400

    FlowerShop

--- module-1/student-assessmentFLower/java/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/student-assessmentFLower/java/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -4,6 +4,28 @@ public class Module1CodingAssessment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		FlowerShop flowerShop =
+				new FlowerShop("Get Well bouquet", 6);
+
+		FlowerShop rachellesOrder =
+				new FlowerShop("Happy Day", 12);
+
+		System.out.println("Object #1: ");
+		System.out.println("Type: " + flowerShop.getBouquetType() + " Number of Roses: " +
+				flowerShop.getNumberOfRoses());
+		System.out.println("Subtotal: " + flowerShop.getSubtotal());
+
+		System.out.println("Object #2: ");
+		System.out.println("Type: " + rachellesOrder.getBouquetType() + " Number of Roses: " +
+				rachellesOrder.getNumberOfRoses());
+		System.out.println("Subtotal: " + rachellesOrder.getSubtotal());
+
+		rachellesOrder.setNumberOfRoses(3);
+		System.out.println(rachellesOrder.toString());
+
+		System.out.println("Delivery for the flowershop object");
+		System.out.println(flowerShop.deliveryTotal(true, "20501"));
+
 
 	}
 
