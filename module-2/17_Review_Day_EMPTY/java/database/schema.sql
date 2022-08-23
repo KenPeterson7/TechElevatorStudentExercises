cf190711bf16de5a7141e7aec765ae398b773fc8
joshuamack <joshua.mack@techelevator.com>
2022-08-23 09:40:20 -0400

    Pokemon API code. And Zoom recordings.

---
+++ module-2/17_Review_Day_EMPTY/java/database/schema.sql
@@ -0,0 +1,33 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS pokemon;
+
+DROP SEQUENCE IF EXISTS seq_user_id;
+DROP SEQUENCE IF EXISTS seq_pokemon_id;
+
+CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  START WITH 1001
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_pokemon_id
+  INCREMENT BY 1
+  START WITH 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE TABLE users (
+	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
+	username varchar(50) NOT NULL,
+	password_hash varchar(200) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id),
+	CONSTRAINT UQ_username UNIQUE (username)
+);
+
+
+
+COMMIT TRANSACTION;
