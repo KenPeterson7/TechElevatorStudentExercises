541787ff10dff1f264760cc86ee3adc959cb33b6
Ken Peterson <Peterskh13@gmail.com>
2022-09-20 17:46:22 -0400

    Week 12 Day 1 Exercise

--- module-3/16_Web_Services_GET/exercise/src/components/TopicList.vue
+++ module-3/16_Web_Services_GET/exercise/src/components/TopicList.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="topic-list">
     <div v-for="topic in topics" v-bind:key="topic.id" class="topic">
+      <router-link v-bind:to="{name: 'Messages', params: {id: topic.id} }">
       {{ topic.title }}
+      <router-link>
     </div>
   </div>
 </template>
 
 <script>
+import TopicService from '../services/TopicService'
+
 export default {
   name: 'topic-list',
   data() {
     return {
       topics: []
     }
-  }
+  },
+  created() {
+    TopicService.getTopics().then((response) => {
+      this.topics = response.data;
+    });
+  },
 }
 </script>
 
