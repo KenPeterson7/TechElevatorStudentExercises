5a004c90093f9eda2a432358018b15ec99a6fa5f
Ken Peterson <Peterskh13@gmail.com>
2022-09-21 18:55:23 -0400

    Last Exercise

--- module-3/17_Web_Services_POST/exercise/src/components/UpdateTopic.vue
+++ module-3/17_Web_Services_POST/exercise/src/components/UpdateTopic.vue
@@ -25,6 +25,12 @@ export default {
     updateTopic() {
       const topic = { id: this.topicID, title: this.title };
       // call topic service update method
+      topicService.updateTopic(topic.id, topic).then(response => {
+        if (response.status === 200){
+          this.$router.push({name: 'Home'})
+        }
+      })
+      
     }
   },
   created() {
